7f7815678ae4e5d6acc339fd0fba699a84e3936b
Edmond Lin <edmondlinn@gmail.com>
2024-04-23 12:16:00 -0700

    first commit

---
+++ src/main/java/com/edmond/springbootsampleapp/SpringbootsampleappApplication.java
@@ -0,0 +1,13 @@
+package com.edmond.springbootsampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsampleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsampleappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edmond/springbootsampleapp/SpringbootsampleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edmond.springbootsampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsampleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
